5d80147f39c828a41cf08abb37ebad5fadd83acb
StreetStrider <teamfortresslife@gmail.com>
2016-09-15 02:22:21 +0300

    test: dataset.

--- test/one.test.js
+++ test/one.test.js
@@ -4,8 +4,19 @@ var raw = require('knex').raw
 
 var knexconn = require('./util/knexconn')()
 
-knexconn.select(raw('current_timestamp as ts'))
-// .debug()
+knexconn.schema.createTable('dataset', table =>
+{
+	table.integer('n')
+})
+.then(() =>
+{
+	return knexconn('dataset')
+	.insert([ { n: 1 }, { n: 2 }, { n: 3 }])
+})
+.then(() =>
+{
+	return knexconn('dataset').select()
+})
 .then(console.log, console.error)
 
 describe('one', () =>
