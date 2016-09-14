1cb6c9b9363931841c25b12f86829b3c56f3da5f
StreetStrider <teamfortresslife@gmail.com>
2016-09-15 01:22:38 +0300

    add sqlite3, use in-memory DB.

--- test/one.test.js
+++ test/one.test.js
@@ -1,4 +1,20 @@
 
+var knex = require('knex')
+var raw  = knex.raw
+
+var knexconn = knex(
+{
+	client: 'sqlite3',
+	connection:
+	{
+		filename: ':memory:'
+	}
+})
+
+knexconn.select(raw('current_timestamp as ts'))
+// .debug()
+.then(console.log, console.error)
+
 describe('one', () =>
 {
 
