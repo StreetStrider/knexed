e552496209ce3f77a1fd140bf40b587560544346
StreetStrider <teamfortresslife@gmail.com>
2016-09-15 03:26:31 +0300

    one: tidy tests.

--- test/one.test.js
+++ test/one.test.js
@@ -6,23 +6,14 @@ var test_error = require('./util/test-error')
 var kx = require('./util/knexconn')()
 var dataset = require('./util/dataset')
 
-var ds = dataset(kx, table =>
-{
-	table.integer('n')
-},
-[
-	{ n: 1 }, { n: 2 }, { n: 3 }
-])
-
-ds.then(ds =>
-{
-	return ds().select()
-})
-.then(console.log, console.error)
-
 describe('one', () =>
 {
 	var one = require('../one')
+
+	var ds = dataset(kx, table => table.integer('n'),
+	[
+		{ n: 1 }, { n: 2 }, { n: 3 }
+	])
 
 	it('one(rows = 1) → row', () =>
 	{
