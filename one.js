bc4ee67d06351d8e44d4d8f9734716afd39f1422
StreetStrider <teamfortresslife@gmail.com>
2016-09-15 03:28:34 +0300

    one.

--- one.js
+++ one.js
@@ -1,6 +1,17 @@
 /* @flow */
 
-module.exports = function one ()
+module.exports = function one /* ::<T> */ (rows /* :Array<T> */) /* :T */
 {
-	
+	if (rows.length === 1)
+	{
+		return rows[0]
+	}
+	else if (rows.length > 1)
+	{
+		throw new TypeError('knexed/one/more-rows')
+	}
+	else
+	{
+		throw new TypeError('knexed/one/no-rows')
+	}
 }
