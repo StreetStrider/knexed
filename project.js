374b3b3599962699e8d85442cddf5a2f63e340aa
StreetStrider <teamfortresslife@gmail.com>
2016-09-16 20:18:40 +0300

    project.

--- project.js
+++ project.js
@@ -1,6 +1,43 @@
 /* @flow */
 
-module.exports = function project ()
+var extend = Object.assign
+
+module.exports = function project (key /* :string */)
 {
-	return () => ({})
+	/* :: type Projection = Object */
+
+	return (rows /* :Array<Object> */) /* :Projection */ =>
+	{
+		if (! rows.length)
+		{
+			return {}
+		}
+
+		var first = rows[0]
+
+		if (! (key in first))
+		{
+			throw TypeError('knexed/project/key-not-present')
+		}
+
+		var proj = {}
+
+		rows.forEach(row =>
+		{
+			var keyval = row[key]
+
+			if (keyval in proj)
+			{
+				throw TypeError('knexed/project/keyval-duplicate')
+			}
+
+			var value = extend({}, row)
+
+			delete value[key]
+
+			proj[keyval] = value
+		})
+
+		return proj
+	}
 }
