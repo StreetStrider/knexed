d1f96eff3dd0ef984cd9cdf9bddae730d92c2e97
StreetStrider <teamfortresslife@gmail.com>
2016-09-18 01:25:13 +0300

    table: test.

---
+++ test/tx/table.test.js
@@ -0,0 +1,33 @@
+/* @flow */
+
+var expect = require('chai').expect
+
+var kx = require('../util/knexconn')()
+var dataset = require('../util/dataset')
+
+var table = require('../../tx/table')
+
+var ds = dataset.series(kx, 1, 4)
+
+describe('table', () =>
+{
+	it('generates new this table based query', () =>
+	{
+		ds
+		.then(ds =>
+		{
+			var name = ds.tableName
+
+			var t = table(kx, name)
+
+			return t().select().where('n', 1)
+		})
+		.then(rows =>
+		{
+			expect(rows).eql(
+			[
+				{ n: 1 }
+			])
+		})
+	})
+})
