7b47f1372c8942776ce1eb9a5703c5cdde40ec5e
StreetStrider <teamfortresslife@gmail.com>
2016-09-18 02:03:37 +0300

    table: flow type for Transaction.

--- tx/table.js
+++ tx/table.js
@@ -2,7 +2,7 @@
 
 module.exports = function table (kx /* :Knex */, table_name /* :string */)
 {
-	return (tx) =>
+	return (tx /* :?Transaction */) =>
 	{
 		return kx(table_name)
 		.transacting(tx)
