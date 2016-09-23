e5cd267ec9d754ea5e3ac1c5bd2508c5fb2938dd
StreetStrider <teamfortresslife@gmail.com>
2016-09-24 00:20:18 +0300

    join(pred[2]), join(pred[3]).

--- join/join.js
+++ join/join.js
@@ -1,24 +1,75 @@
 /* @flow */
 /* ::
-   type TableRef = Table;
-   type Pred     = string;
-
    import type { Table } from '../tx/table';
+
+   type TableRef = Table;
+
+   export type Predicate
+   =   string
+   | [ string, string ];
 */
 
 module.exports = function join
 (
 	left  /* :TableRef */,
 	right /* :TableRef */,
-	pred  /* :Pred */
+	predicate /* :Predicate */
 )
 /* :Table */
 {
+	var L = left.relname
+	var R = right.relname
+
+	predicate = compile_predicate(L, R, predicate)
+
 	return () =>
 	{
-		var rel_l = left.relname
-		var rel_r = right.relname
-
-		return left().join(rel_r, rel_l + '.' + pred, '=', rel_r + '.' + pred)
+		return apply_predicate(left(), R, predicate)
 	}
+}
+
+
+var isArray  = Array.isArray
+
+function compile_predicate
+(
+	L /* :string*/,
+	R /* :string */,
+	predicate /* :Predicate */
+)
+{
+	if (typeof predicate === 'string')
+	{
+		return [
+			L + '.' + predicate,
+			 '=',
+			R + '.' + predicate
+		]
+	}
+	else if (isArray(predicate))
+	{
+		if (predicate.length === 2)
+		{
+			return [
+				L + '.' + predicate[0],
+				 '=',
+				R + '.' + predicate[1]
+			]
+		}
+		else if (predicate.length === 3)
+		{
+			return [
+				L + '.' + predicate[0],
+				 predicate[1],
+				R + '.' + predicate[2]
+			]
+		}
+	}
+
+	throw new TypeError('knexed/join/wrong-predicate')
+}
+
+function apply_predicate (q, R, predicate)
+{
+	return q.join(R, predicate[0], predicate[1], predicate[2])
 }
