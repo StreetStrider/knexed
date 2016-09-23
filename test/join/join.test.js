46e6a2398b00a19da09530b0a340d83db8812e35
StreetStrider <teamfortresslife@gmail.com>
2016-09-23 21:57:41 +0300

    join: new tests.

--- test/join/join.test.js
+++ test/join/join.test.js
@@ -20,13 +20,21 @@ var ds1 = dataset(kx, table =>
 var ds2 = dataset(kx, table =>
 {
 	table.integer('id').primary()
+	table.integer('id_alt').unique()
 	table.string('mark')
 },
 [
-	{ id: 1, mark: 'M1' },
-	{ id: 3, mark: 'M3' },
-	{ id: 4, mark: 'M4' }
+	{ id: 1, id_alt: 1, mark: 'M1' },
+	{ id: 3, id_alt: 2, mark: 'M3' },
+	{ id: 4, id_alt: 3, mark: 'M4' }
 ])
+
+var expected_resultset =
+[
+	{ id: 1, name: 'FOO', id_alt: 1, mark: 'M1' },
+	{ id: 3, name: 'BAZ', id_alt: 2, mark: 'M3' }
+]
+
 
 var ready = Promise.all([ ds1, ds2 ])
 var ready_tables = ready
@@ -44,7 +52,7 @@ var table = require('../../tx/table')
 
 describe('join', () =>
 {
-	it('works', () =>
+	it('join by single colname', () =>
 	{
 		return ready_tables
 		.then(ready =>
@@ -61,10 +69,54 @@ describe('join', () =>
 				`select * from "${ds1.relname}" inner join "${ds2.relname}"` +
 				` on "${ds1.relname}"."id" = "${ds2.relname}"."id"`)
 
+			return expect_select(q, expected_resultset)
+		})
+	})
+
+	it('join by colname pair [ id, id ]', () =>
+	{
+		return ready_tables
+		.then(ready =>
+		{
+			var ds1 = ready[0]
+			var ds2 = ready[1]
+
+			var j = join(ds1, ds2, [ 'id', 'id' ])
+
+			var q = j()
+
+			expect(q.toQuery())
+			.equal(
+				`select * from "${ds1.relname}" inner join "${ds2.relname}"` +
+				` on "${ds1.relname}"."id" = "${ds2.relname}"."id"`)
+
+			return expect_select(q, expected_resultset)
+		})
+	})
+
+	it('join by colname pair [ id, id_alt ]', () =>
+	{
+		return ready_tables
+		.then(ready =>
+		{
+			var ds1 = ready[0]
+			var ds2 = ready[1]
+
+			var j = join(ds1, ds2, [ 'id', 'id_alt' ])
+
+			var q = j().select('id_alt', 'name', 'mark')
+
+			expect(q.toQuery())
+			.equal(
+				`select "id_alt", "name", "mark" from "${ds1.relname}"` +
+				` inner join "${ds2.relname}"` +
+				` on "${ds1.relname}"."id" = "${ds2.relname}"."id_alt"`)
+
 			return expect_select(q,
 			[
-				{ id: 1, name: 'FOO', mark: 'M1' },
-				{ id: 3, name: 'BAZ', mark: 'M3' }
+				{ id_alt: 1, name: 'FOO', mark: 'M1' },
+				{ id_alt: 2, name: 'BAR', mark: 'M3' },
+				{ id_alt: 3, name: 'BAZ', mark: 'M4' }
 			])
 		})
 	})
